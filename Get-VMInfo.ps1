04efe81133febae986edce65214a8ffba75b2faf
Honeycuttj <honeycutt.james@gmail.com>
2019-02-12 21:11:31 -0500

    Initial File upload
    Change IP address to your vCenter Server and/or VMWare Host.  Change the file share path to your file share.  Script needs to be updated to function and built into a tool

---
+++ Get-VMInfo.ps1
@@ -0,0 +1,12 @@
+﻿Add-PSSnapin VMware.VimAutomation.core
+
+
+#Connect-VIServer 55.195.95.49
+Connect-VIServer 55.195.251.50
+
+$pwd = Get-Content C:\Scripts\VMWare_Scripts\TaskSchedualCred
+$cred = New-Object System.Management.Automation.PSCredential "ng\svc.nccsupport.ngpe", $pwd
+$Date = get-date -DisplayHint date -Format ddMMMyy
+
+
+get-vm -name * | export-csv "\\NGPEA7-FL0-1\InfoStore\Information Technology Division\Private\NCC\DIACAP\Servers\ServerList_$Date.csv"
