e6188aa369340289a2ffcd09f739e6e049c95608
Honeycuttj <honeycutt.james@gmail.com>
2020-03-19 21:50:30 -0400

    Re-established this repository

--- Get-VMInfo.ps1
+++ Get-VMInfo.ps1
@@ -1,12 +1,13 @@
 ﻿Add-PSSnapin VMware.VimAutomation.core
+$VIServerIP = 1.1.1.1
+$FilePath = "/Path/to/Store/The/File_$date.csv"
 
-
-#Connect-VIServer 55.195.95.49
-Connect-VIServer 55.195.251.50
+#Connect-VIServer $VIServerIP
+Connect-VIServer $VIServerIP
 
 $pwd = Get-Content C:\Scripts\VMWare_Scripts\TaskSchedualCred
-$cred = New-Object System.Management.Automation.PSCredential "ng\svc.nccsupport.ngpe", $pwd
+#$cred = New-Object System.Management.Automation.PSCredential "myServiceAccount", $pwd
 $Date = get-date -DisplayHint date -Format ddMMMyy
 
 
-get-vm -name * | export-csv "\\NGPEA7-FL0-1\InfoStore\Information Technology Division\Private\NCC\DIACAP\Servers\ServerList_$Date.csv"
+get-vm -name * | export-csv $FilePath
